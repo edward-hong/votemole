f490a5b8ba516d005d2de1b5554a21ec8dd37d65
Edward Hong <edward.hong527@gmail.com>
2020-07-16 00:38:37 +1200

    reconfigure folder structure

---
+++ server/models/routes/authRoutes.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const passport = require('passport')
+
+const router = express.Router()
+
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+)
+
+router.get('/google/callback', passport.authenticate('google'))
+
+router.get('/facebook', passport.authenticate('facebook'))
+
+router.get('/facebook/callback', passport.authenticate('facebook'))
+
+router.get('/twitter', passport.authenticate('twitter'))
+
+router.get('/twitter/callback', passport.authenticate('twitter'))
+
+router.get('/github', passport.authenticate('github'))
+
+router.get('/github/callback', passport.authenticate('github'))
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.send(req.user)
+})
+
+router.get('/current_user', (req, res) => {
+  res.send(req.user)
+})
+
+module.exports = router
