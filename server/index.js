1bc0686a6d1d5010049bd797dacf05639fe5c99a
Edward Hong <edward.hong527@gmail.com>
2020-07-15 23:03:21 +1200

    google oauth

--- server/index.js
+++ server/index.js
@@ -1,6 +1,34 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+
+const authRoutes = require('./routes/authRoutes')
+
+require('./models/User')
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 const app = express()
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY],
+  }),
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./services/passport')
+
+app.use('/auth', authRoutes)
 
 app.get('/', (req, res) => {
   res.send('Hi there')
