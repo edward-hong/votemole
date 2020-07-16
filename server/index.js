8b966fabee0eca42e7bafaea355f6f215f82c537
Edward Hong <edward.hong527@gmail.com>
2020-07-17 00:12:28 +1200

    update deployment configuration

--- server/index.js
+++ server/index.js
@@ -30,9 +30,13 @@ require('./services/passport')
 
 app.use('/auth', authRoutes)
 
-app.get('/', (req, res) => {
-  res.send('Hi there')
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
